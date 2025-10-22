63d797d12d415624d91e28158a6130d9a5779948
Hayat Ay <250541055@firat.edu.tr>
2025-10-22 23:46:18 +0300

    Update UcMesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,8 @@
+public class UcMesaj {
+     public static void main(String[] args) {
+        System.out.println("Java'ya hoşgeldiniz");
+        System.out.println("Bilgisayar Bilimine hoşgeldiniz");
+        System.out.println("Proglama eğlencelidir");
+     }
+
+}
