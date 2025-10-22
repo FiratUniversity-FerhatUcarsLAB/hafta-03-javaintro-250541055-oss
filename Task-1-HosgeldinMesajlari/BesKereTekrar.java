30feb3ff52465923d5510512aa41e386e408b5b1
Hayat Ay <250541055@firat.edu.tr>
2025-10-22 23:06:19 +0300

    Update BesKereTekrar.java

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,10 @@
+public class beşkereyazma {
+    public static void main(String[] args ) {
+
+        System.out.println("JAVA'ya hoşgeldiniz");
+        System.out.println("JAVA'ya hoşgeldiniz");
+        System.out.println("JAVA'ya hoşgeldiniz");
+        System.out.println("JAVA'ya hoşgeldiniz");
+        System.out.println("JAVA'ya hoşgeldiniz"); 
+    }
+}
