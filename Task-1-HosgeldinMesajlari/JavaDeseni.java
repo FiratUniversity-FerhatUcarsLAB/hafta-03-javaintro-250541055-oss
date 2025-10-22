19097460671f1182c6aa652ec6f6c7c3ba40a69a
Hayat Ay <250541055@firat.edu.tr>
2025-10-22 23:29:31 +0300

    Update JavaDeseni.java

---
+++ Task-1-HosgeldinMesajlari/JavaDeseni.java
@@ -0,0 +1,9 @@
+public class javadeseni {
+     public static void main(String[] args) {
+
+     System.out.println("      J A V A ");
+     System.out.println("    J A A V V A A");
+     System.out.println("  J J A A A A V V A A A A");
+     System.out.println("    J A A V V A A ");
+    }
+}
