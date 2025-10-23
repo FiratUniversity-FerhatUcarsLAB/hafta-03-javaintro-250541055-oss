cc0d94f8ad6427c02b93699a4b0d7b76a946556b
Hayat Ay <250541055@firat.edu.tr>
2025-10-23 12:36:48 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,20 @@
+public class İsimKartı {
+    public static void main(String[] Args) {
+
+        String ad = "Hayat";
+        String soyad = "Ay";
+        String ogrno = "2550541055";
+        String bolum = "Yazılım Muhendisligı";
+
+        System.out.println("===================");
+        System.out.println("OĞRENCİ BİLGİLERİ");
+        System.out.println("==================");
+        System.out.println("ad    " + ad);
+        System.out.println("soyad   " + soyad);
+        System.out.println("ogrno    " + ogrno);
+        System.out.println("bolum     " + bolum);
+        System.out.println("==================");
+
+
+    }
+}
