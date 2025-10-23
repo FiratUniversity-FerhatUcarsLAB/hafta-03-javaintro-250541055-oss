b60a545a0e81c2bf1d20c9517e3ee2e0c9149797
Hayat Ay <250541055@firat.edu.tr>
2025-10-23 18:07:37 +0300

    Update CarpimTablosu.java

---
+++ Task-5-TabloDesenleri/CarpimTablosu.java
@@ -0,0 +1,37 @@
+public class CarpımTablosu {
+    public static void main(String[] args) {
+
+    System.out.println("2'nin Çarpım Tablosu");
+System.out.println("---------------------");
+
+int a1 = 1;
+System.out.println("2 x " + a1 + " = " + (2 * a1));
+
+int a2 = 2;
+System.out.println("2 x " + a2 + " = " + (2 * a2));
+
+int a3 = 3;
+System.out.println("2 x " + a3 + " = " + (2 * a3));
+
+int a4 = 4;
+System.out.println("2 x " + a4 + " = " + (2 * a4));
+
+int a5 = 5;
+System.out.println("2 x " + a5 + " = " + (2 * a5));
+
+int a6 = 6;
+System.out.println("2 x " + a6 + " = " + (2 * a6));
+
+int a7 = 7;
+System.out.println("2 x " + a7 + " = " + (2 * a7));
+
+int a8 = 8;
+System.out.println("2 x " + a8 + " = " + (2 * a8));
+
+int a9 = 9;
+System.out.println("2 x " + a9 + " = " + (2 * a9));
+
+int a10 = 10;
+System.out.println("2 x " + a10 + " = " + (2 * a10));
+    }
+}
