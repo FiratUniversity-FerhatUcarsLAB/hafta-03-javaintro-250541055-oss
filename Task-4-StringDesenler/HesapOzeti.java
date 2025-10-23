255e4de9ac5fb56d584d4ed37adfa03348fa8d91
Hayat Ay <250541055@firat.edu.tr>
2025-10-23 15:46:16 +0300

    Update HesapOzeti.java

---
+++ Task-4-StringDesenler/HesapOzeti.java
@@ -0,0 +1,19 @@
+public class HesapOzeti {
+    public static void main(String[] args) {
+
+        System.out.println("====HESAP OZETİ====");
+        System.out.println("Urun\t\tMiktar\tFiyat");
+        System.out.println("---------------------------");
+
+        System.out.println("Ekmek\t\t2\t10 TL");
+        System.out.println("Süt\t\t1\t25 TL");
+        System.out.println("Yumurta\t\t1\t50 TL");
+        System.out.println("Makarna\t\t3\t45 TL");
+
+        System.out.println("----------------------------");
+        System.out.println("Toplam:\t\t\t130 TL");
+
+
+
+    }
+}
