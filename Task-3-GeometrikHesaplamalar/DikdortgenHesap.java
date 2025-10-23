a06dc76c984243644cd4ce3bcdd5d930487bbf39
Hayat Ay <250541055@firat.edu.tr>
2025-10-23 11:07:10 +0300

    Update DikdortgenHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,10 @@
+public class DikdortgenHesap {
+    public static void main(String[] args) {
+
+    double en = 4.5;
+    double boy = 7.9;
+    double alan = en*boy;
+    double çevre = en + boy + en +boy;
+
+    System.out.println("alan" + alan);
+    System.out.println("çevre" + çevre);
