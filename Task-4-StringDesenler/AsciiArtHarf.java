7f214ffae954124d8a13241b1c4a482565fa2316
Hayat Ay <250541055@firat.edu.tr>
2025-10-23 12:48:36 +0300

    Update AsciiArtHarf.java

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,11 @@
+public class AsciiArtHarf {
+    public static void main(String[] args) {
+
+    int  h=104, a=97, y=121 ,t=116;
+       
+    System.out.printf("%c%c%c%c%c" , h , a , y , a , t);
+
+   }
+    
+
+}
